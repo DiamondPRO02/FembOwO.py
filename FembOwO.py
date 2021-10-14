56a9b696da4e543365699e5a2def00a53d2f4a80
DiamondPRO02 <kovacsdanielmark@gmail.com>
2021-10-14 20:45:48 +0200

    -

--- FembOwO.py
+++ FembOwO.py
@@ -2,11 +2,15 @@ import discord
 import logging
 
 client = discord.Client()
-logging.basicConfig(level=logging.INFO)
 
-@client.event
-async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
+#logging.basicConfig(level=logging.INFO)
+#file logging
+logger = logging.getLogger('discord')
+#CRITICAL, ERROR, WARNING, INFO and DEBUG
+logger.setLevel(logging.INFO)
+handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+logger.addHandler(handler)
 
 @client.event
 async def on_message(message):
@@ -14,6 +18,11 @@ async def on_message(message):
         return
 
     if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
+        await message.channel.send('Hello!', mention_author=True)
+        
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
 
-client.run('NO')
+#token
+client.run('token')
