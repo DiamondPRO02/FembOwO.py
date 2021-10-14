ef793b662b85e0232e1db6cb9ff688f3b65f12bc
DiamondPRO02 <kovacsdanielmark@gmail.com>
2021-10-14 22:03:15 +0200

    config implementation

--- FembOwO.py
+++ FembOwO.py
@@ -1,7 +1,15 @@
 import discord
 import logging
+import json
+from discord.ext import commands
+from discord.utils import get
 
 client = discord.Client()
+
+#json config load
+with open('config.json', 'r') as json_config:
+    config = json.load(json_config)
+json_config.close
 
 #logging.basicConfig(level=logging.INFO)
 #file logging
@@ -19,10 +27,23 @@ async def on_message(message):
 
     if message.content.startswith('$hello'):
         await message.channel.send('Hello!', mention_author=True)
-        
+        print('hello test success!')
+
+#NOT WORKING
+@client.event
+async def on_member_join(member):
+    guild = member.guild
+    print('welcome member #1 test success!')
+    if guild.system_channel is not None:
+        to_send = 'Welcome {member.mention} to {guild.name}!'
+        await guild.system_channel.send(to_send)
+        print('welcome member #2 test success!')
+
 @client.event
 async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
+    print('We have logged in as {0.user} (ID: {0.user.id}) '.format(client))
+    print('----------------------------------------------------------------------')
 
 #token
-client.run('token')
+token = config.get("token")
+client.run(token)
