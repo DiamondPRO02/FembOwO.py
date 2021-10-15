64924761de0ac8127b8664467106d0f7ef3c92cd
DiamondPRO02 <kovacsdanielmark@gmail.com>
2021-10-15 20:17:10 +0200

    -

--- FembOwO.py
+++ FembOwO.py
@@ -28,6 +28,7 @@ async def on_message(message):
     prefix = config.get("prefix")
     if message.author.id == client.user.id:
         return
+#write if prefix !== message content startwith => return
     if message.content.startswith(prefix + 'hello'):
         await message.channel.send('Hello!', mention_author=True)
         print('hello test success!')
