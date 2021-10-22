0c6e1024cbb713037bee137c483fe161d1f8b5cf
DiamondPRO02 <kovacsdanielmark@gmail.com>
2021-10-22 20:46:02 +0200

    -

--- FembOwO.py
+++ FembOwO.py
@@ -5,7 +5,6 @@ import datetime
 from discord.ext import commands
 from discord.utils import get
 
-client = discord.Client()
 #json config load
 with open('config.json', 'r') as json_config:
     config = json.load(json_config)
@@ -23,39 +22,37 @@ handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w'
 handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
 logger.addHandler(handler)
 
-@client.event
+@bot.event
 async def on_message(message):
     prefix = config.get("prefix")
-    if message.author.id == client.user.id:
-        return
-    if not message.content.startswith(prefix):
-        print('prefix fail test success!')
-        return
-#write if prefix !== message content startwith => return
-    if message.content.startswith(prefix + 'hello'):
-        await message.channel.send('Hello!', mention_author=True)
-        print('hello test success!')
-        return
-    if message.content.startswith(prefix + 'time'):
-        start_time = datetime.datetime.now()
-        await message.channel.send('The time is:', start_time)
-        print('timetelling test success!')
+    if message.author.id == bot.user.id:
         return
 """
+@bot.command()
+async def ping(ctx):
+    await ctx.send(f'Ping: {round(bot.latency * 1000)}ms')
+@bot.command()
+async def helping(ctx):
+    embed = discord.Embed(
+        title = 'Help page',
+        description = 'This is the help page (WIP)'
+    )
+    embed.set_image(url='https://discordemoji.com/assets/emoji/2788_stupid.png')
+    embed.set_footer(text='Laget av Alexander Gnauck 3ELDEA')
+
+    await ctx.send(embed = embed)
+    pass
 @bot.command()
 async def hello(ctx):
     await ctx.send('Hello!', mention_author=True)
     print('hello test success!')
-    pass
-@commands.command()
+@bot.command()
 async def time(ctx):
     date_time = datetime.datetime.now()
     await ctx.send('The time is:', date_time)
     print('timetelling test success!')
-    pass
-bot.add_command(time)
 #NOT WORKING
-@client.event
+@bot.event
 async def on_member_join(member):
     guild = member.guild
     print('welcome member #1 test success!')
@@ -63,23 +60,15 @@ async def on_member_join(member):
         to_send = 'Welcome {member.mention} to {guild.name}!'
         await guild.system_channel.send(to_send)
         print('welcome member #2 test success!')
-#Fuck me
-class MyClient(discord.Client):
-    async def on_member_join(self, member):
-        guild = member.guild
-        print('welcome member #11 test success!')
-        if guild.system_channel is not None:
-            to_send = 'Welcome {0.mention} to {1.name}!'.format(member, guild)
-            await guild.system_channel.send(to_send)
-            print('welcome member #22 test success!')
-#reeeeeeeeeeeeeeeeeeeeee
 
-@client.event
+#activated when bot ready
+@bot.event
 async def on_ready():
     start_time = datetime.datetime.now()
-    print('We have logged in as {0.user} (ID: {0.user.id}) \nTime:'.format(client), start_time, '\nPrefix:', prefix)
+    print('We have logged in as {0.user} (ID: {0.user.id}) \nTime:'.format(bot), start_time, '\nPrefix:', prefix)
     print('----------------------------------------------------------------------')
+    await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.playing, name="ur mum"))
 
 #token
 token = config.get("token")
-client.run(token)
+bot.run(token)
